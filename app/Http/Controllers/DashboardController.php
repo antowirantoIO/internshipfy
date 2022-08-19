a774647eb3ac4372097e69e646f9be0f45068c29
Anto Wiranto <antowrnto11@gmail.com>
2022-08-19 23:20:53 +0700

    cleaning unused files 🏃🏻‍♂️

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -6,19 +6,10 @@ use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
-  // Dashboard - Analytics
-  public function dashboardAnalytics()
-  {
-    $pageConfigs = ['pageHeader' => false];
+    public function index()
+    {
+        $pageConfigs = ['pageHeader' => false];
 
-    return view('/content/dashboard/dashboard-analytics', ['pageConfigs' => $pageConfigs]);
-  }
-
-  // Dashboard - Ecommerce
-  public function dashboardEcommerce()
-  {
-    $pageConfigs = ['pageHeader' => false];
-
-    return view('/content/dashboard/dashboard-ecommerce', ['pageConfigs' => $pageConfigs]);
-  }
+        return view('dashboard/index', ['pageConfigs' => $pageConfigs]);
+    }
 }
--- resources/views/content/pages/buy-now.blade.php
+++
@@ -1,5 +0,0 @@
-{{-- BEGIN: Buy Now --}}
-<div class="buy-now">
-    <a href="https://1.envato.market/vuexy_admin" target="_blank" class="btn btn-danger">Buy Now</a>
-</div>
-{{-- End: Buy Now --}}
